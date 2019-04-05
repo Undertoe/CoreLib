2270c440679e5847688482ac1cda85ae17d205f9
Terryn Fredrickson <terryn007@hotmail.com>
2019-04-05 14:29:12 -0500

    Editing install script
    going to figure out how to set this up better.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ rm /??/libs/lib64/libCoreLib.a
 rm /??/libs/lib64/libCoreLibd.a
 cd build/
 make clean
-/opt/cmake-3.13.3-Linux-x86_64/bin/cmake .. -D CMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:Path=/??/libs && make all install
+cmake .. -D CMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:Path=/??/libs && make all install
 make clean
-/opt/cmake-3.13.3-Linux-x86_64/bin/cmake .. -D CMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX:Path=/??/libs && make all install
+cmake .. -D CMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX:Path=/??/libs && make all install
 
